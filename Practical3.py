a284fc408d28066d47ab937df424b0b78dcf12d5
TRIVENI SAWAT <trivenisawat55@gmail.com>
2026-07-16 00:19:39 +0530

    Add function to check if a number is even or odd

---
+++ Practical3.py
@@ -0,0 +1,15 @@
+def is_even(number):
+  if number % 2==0:
+    return True 
+  else:
+    return False
+number=int(input("enter a number))
+if is_even(number):
+     print(f"{number} is even")
+else:
+     print(f"{number} is odd")
+      
+                 
+                 
+                
+                 
